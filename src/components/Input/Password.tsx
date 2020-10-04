fba3ff1d86480ae28dc22c39fe030ddd79ede690
潘博 <1350210100@qq.com>
2020-10-04 17:16:22 +0800

    Update: Input.Password add placeholder

--- src/components/Input/Password.tsx
+++ src/components/Input/Password.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import _ from 'lodash'
 import Input, { IInputProps, InputTypes } from './Input'
 
 const _Password: React.ForwardRefRenderFunction<unknown, IInputProps> = (props, ref) => {
-	const newProps = _.omit(props, ['placeholder'])
-	return <Input {...newProps} ref={ref as any} type={InputTypes.PASSWORD} />
+	return <Input {...props} ref={ref as any} type={InputTypes.PASSWORD} />
 }
 
 const Password = React.memo(React.forwardRef<unknown, IInputProps>(_Password))
